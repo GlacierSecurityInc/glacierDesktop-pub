a18d29db21e63e58301152b4f67b473afa65ccfe
Chance Snow <git@chancesnow.me>
2021-04-09 14:21:17 -0500

    Integrate Cognito service into the Login controller

--- app/controllers/login-controller.js
+++ app/controllers/login-controller.js
@@ -1,21 +1,29 @@
 let angApp = require(__dirname+'/../init')
 
-angApp.controller('LoginController', function($scope, DesktopService, CredentialsService) {
+angApp.controller('LoginController', function($scope, DesktopService, CognitoService, CredentialsService) {
 
     $scope.help = {}
+    $scope.error = null
 
     $scope.showHelp = (item) => {
         $scope.help[item] = typeof $scope.help[item] === 'undefined' ? true : !$scope.help[item];
     }
 
     $scope.addAccountAndLoginAction = () => {
-        CredentialsService.addCredentials($scope.credentials.connectionManager,
-            $scope.credentials.login,
-            $scope.credentials.password
-        )
-        DesktopService.getCredentialsAndLogin()
-        $scope.accountForm.$setPristine()
-        $scope.accountForm.$setUntouched()
-        $scope.credentials = {}
+        $scope.error = null
+        CognitoService.fetchCredentials($scope.credentials.login, $scope.credentials.password).then(credentials => {
+            CredentialsService.addCredentials(
+                credentials.connectionManager,
+                credentials.login,
+                credentials.password
+            )
+            DesktopService.getCredentialsAndLogin()
+        }).catch(error => {
+            $scope.error = error
+        }).finally(() => {
+            $scope.accountForm.$setPristine()
+            $scope.accountForm.$setUntouched()
+            $scope.credentials = {}
+        })
     }
 })
