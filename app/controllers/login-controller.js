8e2082c0ee4830aeb950cb9304f8d2d1b40ce1a7
Chance Snow <git@chancesnow.me>
2021-04-09 17:09:50 -0500

    Disable login button during login procedure

--- app/controllers/login-controller.js
+++ app/controllers/login-controller.js
@@ -3,6 +3,7 @@ let angApp = require(__dirname+'/../init')
 angApp.controller('LoginController', function($scope, AppInfo, DesktopService, CognitoService, CredentialsService) {
 
     $scope.help = {}
+    $scope.loggingIn = false
     $scope.error = null
 
     $scope.showHelp = (item) => {
@@ -10,6 +11,7 @@ angApp.controller('LoginController', function($scope, AppInfo, DesktopService, C
     }
 
     $scope.addAccountAndLoginAction = () => {
+        $scope.loggingIn = true
         $scope.error = null
         CognitoService.fetchCredentials($scope.credentials.login, $scope.credentials.password).then(credentials => {
             CredentialsService.addCredentials(
@@ -21,6 +23,7 @@ angApp.controller('LoginController', function($scope, AppInfo, DesktopService, C
         }).catch(error => {
             $scope.error = error
         }).finally(() => {
+            $scope.loggingIn = false
             $scope.accountForm.$setPristine()
             $scope.accountForm.$setUntouched()
             $scope.credentials = {}
