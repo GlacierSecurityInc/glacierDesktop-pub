4697887876aac028580e009c4b4586a6a8ea898f
Max Isom <hi@maxisom.me>
2021-07-12 10:54:43 -0500

    Update login page styling & show correct page after logout

--- app/services/app-state-service.js
+++ app/services/app-state-service.js
@@ -13,6 +13,17 @@ angApp.factory('AppStateService', [ '$rootScope', ($rootScope) => {
         stateService.previousState = typeof stateService.state !== 'undefined' ?
             stateService.state : stateService.APP_STATE_DEFAULT
         stateService.state = state
+
+        // Need to hide the default login page
+        const container = document.querySelector('.flyout')
+        if (container) {
+            if (state === stateService.APP_STATE_LOGIN) {
+                container.style.display = 'none';
+            } else {
+                container.style.display = 'block';
+            }
+        }
+
         $rootScope.$broadcast('app:state:changed', stateService.state)
     }
 
