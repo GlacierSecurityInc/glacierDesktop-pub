920e13c6b9439a31a04597e984924126c9ea9b34
Max Isom <hi@maxisom.me>
2021-07-19 09:58:51 -0500

    Temporarily removes update checking

--- app/init.js
+++ app/init.js
@@ -11,9 +11,9 @@ const angApp = angular.module('app', [])
 angApp.constant('AppInfo', {
     APP_NAME: 'Glacier Desktop',
     APP_VERSION: `v${appVersion}`,
-    APP_HOME: 'https://github.com/conversejs/converse-desktop',
-    APP_RELEASES_CHECK_URL: 'https://api.github.com/repos/conversejs/converse-desktop/releases',
-    APP_RELEASES_URL: 'https://github.com/conversejs/converse-desktop/releases',
+    APP_HOME: 'https://github.com/GlacierSecurityInc/glacierDesktop',
+    // APP_RELEASES_CHECK_URL: 'https://api.github.com/repos/conversejs/converse-desktop/releases',
+    // APP_RELEASES_URL: 'https://github.com/conversejs/converse-desktop/releases',
     CONVERSE_VERSION: converseVersion.replace('^', 'v'),
     ELECTRON_VERSION: electronVersion.replace('^', 'v'),
     GLACIER_BOSH: 'http://geweb.glaciersec.cc/bosh'
