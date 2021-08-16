bd85412cd3e33e4a575fecfc59b4d1d98b970671
Max Isom <hi@maxisom.me>
2021-08-16 11:22:18 -0500

    Show build metadata on about page

--- app/init.js
+++ app/init.js
@@ -1,21 +1,15 @@
 const angular = require('angular')
 const path = require('path')
 
-const {
-    appVersion,
-    electronVersion,
-    converseVersion,
-} = require(path.join(__dirname, 'metadata.json'))
+const metadata = require(path.join(__dirname, 'metadata.json'))
 
 const angApp = angular.module('app', [])
 angApp.constant('AppInfo', {
     APP_NAME: 'Glacier Desktop',
-    APP_VERSION: `v${appVersion}`,
     APP_HOME: 'https://github.com/GlacierSecurityInc/glacierDesktop',
     // APP_RELEASES_CHECK_URL: 'https://api.github.com/repos/conversejs/converse-desktop/releases',
     // APP_RELEASES_URL: 'https://github.com/conversejs/converse-desktop/releases',
-    CONVERSE_VERSION: converseVersion.replace('^', 'v'),
-    ELECTRON_VERSION: electronVersion.replace('^', 'v'),
+    metadata,
     CONNECTION_MANAGER: 'wss://discover' //placeholder, using discovery method
 });
 
