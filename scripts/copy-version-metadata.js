bdc6a842a392dc09bb76e2b26d7e887f3d07a401
Max Isom <hi@maxisom.me>
2021-08-19 11:16:32 -0500

    Add back Converse & Electron versions, format info

--- scripts/copy-version-metadata.js
+++ scripts/copy-version-metadata.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const fs = require('fs')
 const appDir = path.join(__dirname, '..', 'app')
+const package = require(path.join(__dirname, '../package.json'))
+const electronVersion = package.devDependencies.electron
+const conversePackage = require(path.join(__dirname, '../libs/vendor/converse.js/package.json'))
+const converseVersion = conversePackage.version
 
 const out = {
     CLOSES: process.env.CLOSES,
@@ -8,7 +12,9 @@ const out = {
     CONVERSE_BRANCH: process.env.CONVERSE_BRANCH,
     GLACIER_SHA: process.env.GLACIER_SHA,
     CONVERSE_SHA: process.env.CONVERSE_SHA,
-    VERSION: process.env.VERSION,
+    VERSION: package.version,
+    ELECTRON_VERSION: electronVersion,
+    CONVERSE_VERSION: converseVersion,
     BUILD_NUMBER: process.env.BUILD_NUMBER
 };
 
